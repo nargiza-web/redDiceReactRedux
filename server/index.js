c39b4f8e77a45a3e26e581b605d147f66cc9d1f2
mehmet ozek <mehmetozek@mehmets-MacBook-Air.local>
2020-02-09 08:56:09 -0600

    set up

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const webpack= require('webpack')
+const webpackConfig = require('../webpack.config.dev')
+
+
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'))
---
+++ webpack.config.dev.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+export default{
+  entry: path.join(__dirname, '/client/index.js'),
+  output: {
+    path: '/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'client'),
+        loaders: ['babel']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['','.js']
+  }
+}
